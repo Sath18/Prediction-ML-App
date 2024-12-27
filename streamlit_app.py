64cdc7337fcf3ea46e323cdd470a9d4b1db3c6bc
Sath18 <sathcodes18@gmail.com>
2024-12-27 21:48:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X = df.drop('species', axis=1)
-  X
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data = df, x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
@@ -56,7 +56,7 @@ with st.sidebar:
          'sex' : gender}
 
   input_df = pd.DataFrame(data, index = [0])
-  input_penguins = pd.concat([input_df, X], axis = 0)
+  input_penguins = pd.concat([input_df, X_raw], axis = 0)
 
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix = encode)
