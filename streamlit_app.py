d5ca5d42ffe7f160443213724e5a71bd2b4c0328
Sath18 <sathcodes18@gmail.com>
2024-12-27 13:20:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('🤖 Machine Learning Prediction App')
 
 st.info('This is a ML Prediction App')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 
 df
