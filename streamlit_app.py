bc28f7ae7ddc9348e6c9b66abc613577185ba4dc
Sath18 <sathcodes18@gmail.com>
2024-12-27 19:10:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,6 @@ with st.sidebar:
     'Island',
     ('Biscoe','Dream','Torgersen')
   )
-  gender = st.selectbox(
-    'Gender',
-    ('Male','Female')
-  )
   bill_length_mm = st.slider(
     'Bill Length (mm)',
     32.1, 59.6, 43.9
@@ -47,3 +43,18 @@ with st.sidebar:
     'Body Mass (g)',
     2700.0, 6300.0, 4207.0
   )
+  gender = st.selectbox(
+    'Gender',
+    ('Male','Female')
+  )
+
+  data = {'island' : island,
+         'bill_length_mm' : bill_length_mm,
+         'bill_depth_mm' : bill_depth_mm,
+         'flipper_length_mm' : flipper_length_mm,
+         'body_mass_g' : body_mass_g,
+         'gender' : gender}
+
+  input_df = pd.DataFrame(data, index=[0])
+
+input_df
